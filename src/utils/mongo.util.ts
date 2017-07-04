095c1b2e9197f56dc8312734ec146558d155519b
VinHty <791606691@qq.com>
2017-07-04 16:58:29 +0800

    delete some useless files ,modify the package.json file.

--- src/utils/mongo.util.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-const url = 'mongodb://127.0.0.1:27017/test';
-
-const mongo = mongoose.createConnection(url);
-
-mongo.on('connect', () => console.log(`mongoDB连接成功 ${url}`));
-
-export default mongo;
