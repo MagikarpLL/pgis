a02be63667f98f1cd63efe9412db96d8acb64e1c
leizhen <3422785094@qq.com>
2017-07-13 15:49:14 +0800

    11

--- src/controller/inputdata/datainput.controller.ts
+++ src/controller/inputdata/datainput.controller.ts
@@ -2,7 +2,7 @@ import * as Router from 'koa-router';
 import { Context } from '../../utils/koa.util';
 import { route, required, log, HttpMethod, DataType } from '../../addon/route';
 import { lineToHump } from '../../utils/split.util';
-import { residence } from "../../biz/data-input.biz";
+import { residence, populationDetailed, population, overSeaRelative} from "../../biz/data-input.biz";
 
 export default class DatainputController {
 
@@ -13,10 +13,10 @@ export default class DatainputController {
         unless: true,
     })
     @log
-    async create(ctx: Context, next: Function): Promise<void> {
+    async residence(ctx: Context, next: Function): Promise<void> {
         try {
             console.log(ctx.request.body);
-            let result = await residence(ctx.request.body, ctx.db);
+            let result = await residence(ctx.request.body, ctx.db, ctx.sql);
             console.log(result);
             ctx.success(result, 'success');
         } catch (e) {
@@ -24,4 +24,62 @@ export default class DatainputController {
             ctx.error('error', e);
         }
     }
+
+        //数据上传-----人口详细信息表
+    @route({
+        path: '/populationDetailed',
+        method: HttpMethod.POST,
+        unless: true,
+    })
+    @log
+    async populationDetailed(ctx: Context, next: Function): Promise<void> {
+        try {
+            console.log(ctx.request.body);
+            let result = await populationDetailed(ctx.request.body, ctx.db, ctx.sql);
+            console.log(result);
+            ctx.success(result, 'success');
+        } catch (e) {
+            console.error(e);
+            ctx.error('error', e);
+        }
+    }
+
+        //数据上传-----人口表
+    @route({
+        path: '/population',
+        method: HttpMethod.POST,
+        unless: true,
+    })
+    @log
+    async population(ctx: Context, next: Function): Promise<void> {
+        try {
+            console.log(ctx.request.body);
+            let result = await population(ctx.request.body, ctx.db, ctx.sql);
+            console.log(result);
+            ctx.success(result, 'success');
+        } catch (e) {
+            console.error(e);
+            ctx.error('error', e);
+        }
+    }
+
+        //数据上传-----海外亲属表
+    @route({
+        path: '/overSeaRelative',
+        method: HttpMethod.POST,
+        unless: true,
+    })
+    @log
+    async overSeaRelative(ctx: Context, next: Function): Promise<void> {
+        try {
+            console.log(ctx.request.body);
+            let result = await overSeaRelative(ctx.request.body, ctx.db, ctx.sql);
+            console.log(result);
+            ctx.success(result, 'success');
+        } catch (e) {
+            console.error(e);
+            ctx.error('error', e);
+        }
+    }
+
 }
