6e93fc932e6783a4401ccdd022a75133f888b5f5
leizhen <3422785094@qq.com>
2017-07-24 16:05:59 +0800

    1

--- src/biz/countSelect.biz.ts
+++ src/biz/countSelect.biz.ts
@@ -4,6 +4,19 @@ import UUID from "../utils/uuid.util";
 import Date from '../utils/date.util';
 import { SQL } from '../utils/sql.util';
 
+//countGraph
+export async function countGraph(body: any, db: any, sql: SQL): Promise<any> {
+    let buildingId = body.buildingId;
+    let sqlExec = ` select popu.idNumber, popu.residenceId, popu.name, popu.gender, popu.ethnicity, popu.birthday,
+    popu.educationalDegree, popu.politicalStatus, popu.maritalStatus, popu.relationToHouseholder, popu.typeOfHousehold,
+    popu.ancestralNativePlace, popu.residentialAddress, popu.verificationDate, popu.issueCertificateDate, popu.speciality,
+    popu.pets, popu.workplace, popu.companyProperty, popu.personnelCategory, popu.peopleLivehoodCategory
+    from TB_PROPERTYGIS gis, Tb_Residence res, Tb_Population popu where gis.roomid = res.roomid 
+    and res.householdid = popu.residenceid and gis.buildingId = ${ buildingId}`;
+    const result = await db.query(sqlExec);
+    const jsObj = JSON.parse(result.result);
+    return jsObj;
+}
 
 //buildingId
 export async function building(body: any, db: any, sql: SQL): Promise<any> {
--- src/controller/count/countSelect.controller.ts
+++ src/controller/count/countSelect.controller.ts
@@ -2,10 +2,34 @@ import * as Router from 'koa-router';
 import { Context } from '../../utils/koa.util';
 import { route, required, log, HttpMethod, DataType } from '../../addon/route';
 import { lineToHump } from '../../utils/split.util';
-import { building, unit, room } from "../../biz/countSelect.biz";
+import { building, unit, room, countGraph } from "../../biz/countSelect.biz";
 import { encode, decode } from '../../utils/crypto.util';
 
 export default class CountSelectController {
+
+    //带参统计查询-----building
+    @route({
+        path: '/countGraph',
+        method: HttpMethod.GET,
+        unless: true,
+    })
+    @required({
+        'body': 'value'
+    })
+    @log
+    async countGraph(ctx: Context, next: Function): Promise<void> {
+        try {
+            let body = decode(ctx.request.body.value);
+            body = JSON.parse(body);
+            let result = await countGraph(body, ctx.db, ctx.sql);
+            let fin = encode(JSON.stringify(result));
+            ctx.success(fin, 'success');
+        } catch (e) {
+            console.error(e);
+            ctx.error('error', e);
+        }
+    }
+
 
     //带参统计查询-----building
     @route({
