ba9fb21879aaf803ab6dd88339c0a12525c3da04
leizhen <3422785094@qq.com>
2017-07-25 16:01:35 +0800

    1

--- src/controller/inputdata/datainput.controller.ts
+++ src/controller/inputdata/datainput.controller.ts
@@ -2,7 +2,8 @@ import * as Router from 'koa-router';
 import { Context } from '../../utils/koa.util';
 import { route, required, log, HttpMethod, DataType } from '../../addon/route';
 import { lineToHump } from '../../utils/split.util';
-import { residence, populationDetailed, population, overSeaRelative} from "../../biz/data-input.biz";
+import { residence, populationDetailed, population, overSeaRelative } from "../../biz/data-input.biz";
+import { encode, decode } from '../../utils/crypto.util';
 
 export default class DatainputController {
 
@@ -15,8 +16,9 @@ export default class DatainputController {
     @log
     async residence(ctx: Context, next: Function): Promise<void> {
         try {
-            console.log(ctx.request.body);
-            let result = await residence(ctx.request.body, ctx.db, ctx.sql);
+            let body = decode(ctx.request.body.value);
+            body = JSON.parse(body);
+            let result = await residence(body, ctx.db, ctx.sql);
             console.log(result);
             ctx.success(result, 'success');
         } catch (e) {
@@ -25,7 +27,7 @@ export default class DatainputController {
         }
     }
 
-        //数据上传-----人口详细信息表
+    //数据上传-----人口详细信息表
     @route({
         path: '/populationDetailed',
         method: HttpMethod.POST,
@@ -44,7 +46,7 @@ export default class DatainputController {
         }
     }
 
-        //数据上传-----人口表
+    //数据上传-----人口表
     @route({
         path: '/population',
         method: HttpMethod.POST,
@@ -63,7 +65,7 @@ export default class DatainputController {
         }
     }
 
-        //数据上传-----海外亲属表
+    //数据上传-----海外亲属表
     @route({
         path: '/overSeaRelative',
         method: HttpMethod.POST,
