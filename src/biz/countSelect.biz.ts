f89f65b550051a46097027e65254ea6370bfc700
leizhen <3422785094@qq.com>
2017-07-28 14:37:02 +0800

    小区统计

--- src/biz/countSelect.biz.ts
+++ src/biz/countSelect.biz.ts
@@ -4,6 +4,93 @@ import UUID from "../utils/uuid.util";
 import Date from '../utils/date.util';
 import { SQL } from '../utils/sql.util';
 
+//residentialAreas
+export async function residentialAreas(body: any, db: any, sql: SQL): Promise<any> {
+    let neighborhood = body.neighborhood;
+    let sqlExec = `SELECT   
+       COUNT(CASE  
+               WHEN popu.gender  IN ('男') THEN  
+                '男'  
+             END) 男,
+       COUNT(CASE  
+               WHEN popu.gender  IN ('女') THEN  
+                '女'  
+             END) 女,
+       COUNT(CASE  
+               WHEN popu.maritalstatus  IN ('已婚') THEN  
+                '已婚'  
+             END) 已婚,
+       COUNT(CASE  
+               WHEN popu.maritalstatus  IN ('未婚') THEN  
+                '未婚'  
+             END) 未婚,
+       COUNT(CASE  
+               WHEN popu.educationaldegree  IN ('大学') THEN  
+                '大学'  
+             END) 大学,
+       COUNT(CASE  
+               WHEN popu.educationaldegree  IN ('高中') THEN  
+                '高中'  
+             END) 高中,
+       COUNT(CASE  
+               WHEN popu.educationaldegree  IN ('初中') THEN  
+                '初中'  
+             END) 初中,
+       COUNT(CASE  
+               WHEN popu.educationaldegree  IN ('专科') THEN  
+                '专科'  
+             END) 专科,
+       COUNT(CASE  
+               WHEN popu.educationaldegree  IN ('初中以下') THEN  
+                '初中以下'  
+             END) 初中以下,
+       COUNT(CASE  
+               WHEN popu.politicalstatus  IN ('党员') THEN  
+                '党员'  
+             END) 党员,      
+       COUNT(CASE  
+               WHEN popu.politicalstatus  IN ('团员') THEN  
+                '团员'  
+             END) 团员,               
+       COUNT(CASE  
+               WHEN popu.politicalstatus  IN ('群众') THEN  
+                '群众'  
+             END) 群众,               
+       COUNT(CASE  
+               WHEN popu.typeofhousehold  IN ('非农') THEN  
+                '非农'  
+             END) 非农,               
+       COUNT(CASE  
+               WHEN popu.typeofhousehold  IN ('农业') THEN  
+                '农业'  
+             END) 农业,                                                                                       
+       COUNT(CASE  
+               WHEN popu.ethnicity  IN ('汉族') THEN  
+                '汉族'  
+             END) 汉族,    
+       COUNT(CASE  
+               WHEN popu.ethnicity  IN ('满族') THEN  
+                '满族'  
+             END) 满族,             
+       COUNT(CASE  
+               WHEN popu.ethnicity  IN ('壮族') THEN  
+                '壮族'  
+             END) 壮族,             
+       COUNT(CASE  
+               WHEN popu.ethnicity  IN ('藏族') THEN  
+                '藏族'  
+             END) 藏族,             
+       COUNT(CASE  
+               WHEN popu.ethnicity  IN ('其他民族') THEN  
+                '其他民族'  
+             END) 其他民族                                             
+        from TB_PROPERTYGIS gis, Tb_Residence res, Tb_Population popu where gis.roomid = res.roomid 
+        and res.householdid = popu.residenceid and gis.neighborhood = ${ neighborhood}`;    
+    const result = await db.query(sqlExec);
+    const jsObj = JSON.parse(result.result);
+    return jsObj;
+}
+
 //buildingId
 export async function building(body: any, db: any, sql: SQL): Promise<any> {
     let buildingId = body.buildingId;
--- src/controller/count/countSelect.controller.ts
+++ src/controller/count/countSelect.controller.ts
@@ -2,11 +2,33 @@ import * as Router from 'koa-router';
 import { Context } from '../../utils/koa.util';
 import { route, required, log, HttpMethod, DataType } from '../../addon/route';
 import { lineToHump } from '../../utils/split.util';
-import { building, unit, room } from "../../biz/countSelect.biz";
+import { building, unit, room, residentialAreas } from "../../biz/countSelect.biz";
 import { encode, decode } from '../../utils/crypto.util';
 
 export default class CountSelectController {
 
+    //带参统计查询-----residentialAreas
+    @route({
+        path: '/residentialAreas',
+        method: HttpMethod.POST,
+        unless: true,
+    })
+    @required({
+        'body': 'value'
+    })
+    @log
+    async residentialAreas(ctx: Context, next: Function): Promise<void> {
+        try {
+            let body = decode(ctx.request.body.value);
+            body = JSON.parse(body);
+            let result = await residentialAreas(body, ctx.db, ctx.sql);
+            let fin = encode(JSON.stringify(result));
+            ctx.success(fin, 'success');
+        } catch (e) {
+            console.error(e);
+            ctx.error('error', e);
+        }
+    }
 
     //带参统计查询-----building
     @route({
